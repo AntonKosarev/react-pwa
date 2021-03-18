645ef6e607f7d8db5f7a3690d9ed823b0ac4db52
Anton <kosarev.anton.p@gmail.com>
2021-03-18 18:31:33 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/index.js",
-    output: {path: path.join(__dirname, "./dist"), filename: "index_bundle.js"},
+    output: {path: path.join(__dirname, "/dist"), filename: "index_bundle.js"},
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, use: {loader: "babel-loader" } },
